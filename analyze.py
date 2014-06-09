6d5b967a19f9ffb23c0c7d212b819021e776d286
Moritz Gellner <moritz.gellner@gmail.com>
2014-06-08 20:07:44 -0500

    WIP

--- analyze.py
+++ analyze.py
@@ -1,16 +1,53 @@
-import csv, datetime
+import csv, datetime, sys
 
-users = []
+# users = []
 
-with open('data.csv', 'rU') as csvfile:
-  reader = csv.DictReader(csvfile)
-  users = [ dict([(key,val.strip()) for key,val in row.iteritems()]) for row in reader]
-  print users[0].keys()
+# with open('data.csv', 'rU') as csvfile:
+#   reader = csv.DictReader(csvfile)
+#   users = [ dict([(key,val.strip()) for key,val in row.iteritems()]) for row in reader]
+#   print users[0].keys()
 
-call_time = "9:00 PM"
+# call_time = "9:00 PM"
 
-for user in users:
-  if not user['Birthdate'] or not user['Birthdate'][0:2] != "00": continue
-  birthdate = datetime.datetime.strptime(user['Birthdate'], "%m%d%Y")
-  print birthdate
+# for user in users:
+#   if not user['Birthdate'] or not user['Birthdate'][0:2] != "00": continue
+#   birthdate = datetime.datetime.strptime(user['Birthdate'], "%m%d%Y")
+#   print birthdate
   # if birthdate > datetime.datetime.now() - 
+
+def find_call_time(user):
+	# preprocessing
+	if not user['Birthdate'] or not user['Birthdate'][0:2] != "00":
+		age = -1
+	else:
+		age = (datetime.datetime.now() - datetime.datetime.strptime(user['Birthdate'], "%m%d%Y"))
+		import pdb; pdb.set_trace()
+
+	# find call time
+	pass
+
+DEBUG = True
+
+if __name__ == "__main__":
+
+	if len(sys.argv) > 1:
+		csvname = sys.argv[1]
+	else:
+		csvname = 'data.csv'
+
+	with open(csvname, 'rU') as csvfile:
+  		reader = csv.DictReader(csvfile)
+  		users = [ dict([(key,val.strip()) for key,val in row.iteritems()]) for row in reader]
+  		if DEBUG: print users[0].keys()
+
+	augmented_users = []
+	for user in users:
+		call_time = find_call_time(user)
+		user['Call Time'] = call_time
+		augmented_users.append(user)
+
+  	with open(csvname,'rU') as csvfile:
+  		writer = csv.DictWriter(csvfile,users.keys())
+  		for user in augmented_users:
+  			writer.writerow(user)
+  			if DEBUG: print user
---
+++ utils.py
@@ -0,0 +1,26 @@
+from __future__ import division
+import urllib, json, requests
+
+TZ_API_KEY = 'QWLU5Z73POKS'
+
+def get_tz_for_city(city,state):
+	gmap_api_url = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false" % urllib.quote(city + ", " + state)
+	resp = requests.get(gmap_api_url)
+	try:
+		json_resp = json.loads(resp.text)
+		if 'results' in json_resp:
+			lat = json_resp['results'][0]['geometry']['location']['lat']
+			lng = json_resp['results'][0]['geometry']['location']['lng']
+			tz_api_url = "http://api.timezonedb.com/?lat=%s&lng=%s&key=%s&format=json" % (lat,lng,TZ_API_KEY)
+			resp = requests.get(tz_api_url)
+			json_resp = json.loads(resp.text)
+			print json_resp
+			print json_resp['gmtOffset']
+			return int(json_resp['gmtOffset'])/(60*60)
+
+	except KeyError:
+		print "Key Error in get_tz_for_city"
+		return None
+
+if __name__ == "__main__":
+	print get_tz_for_city('San Jose','CA')
